a5a8fd2dfefa81ad385af6a92fcc7636c6cbf935
Michal Darda <mdarda@pm.me>
2019-10-10 11:13:32 +0200

    WIP

--- app/javascript/packs/game_editor.jsx
+++ app/javascript/packs/game_editor.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
+import Cytoscape from 'cytoscape';
+import dagre from 'cytoscape-dagre';
 
-class MyApp extends React.Component {
-  constructor(props){
-    super(props);
-  }
+Cytoscape.use(dagre);
 
-  render(){
-    // const elements = [
-    //    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    //    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    //    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-    // ];
+function MyApp(props) {
+  // const elements = [
+  //    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+  //    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+  //    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  // ];
 
-    return [
-      <pre>{JSON.stringify(this.props.elements, null, 2)}</pre>,
-      <CytoscapeComponent elements={this.props.elements} style={ { width: '1200px', height: '1200px' } } />
-    ];
-  }
+  return [
+    <CytoscapeComponent
+      elements={props.elements}
+      style={{ width: '600px', height: '600px' }}
+      layout={{name: 'dagre'}}
+    />,
+    <pre>{JSON.stringify(props.elements, null, 2)}</pre>
+  ];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
