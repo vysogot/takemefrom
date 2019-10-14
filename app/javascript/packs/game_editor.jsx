ba44b2fbf03666e2bdd4b89b57eccc8febde0419
Michal Darda <mdarda@pm.me>
2019-10-14 11:38:45 +0200

    Open modal on click

--- app/javascript/packs/game_editor.jsx
+++ app/javascript/packs/game_editor.jsx
@@ -3,15 +3,47 @@ import ReactDOM from "react-dom";
 import dagre from "cytoscape-dagre";
 import CytoscapeComponent from "react-cytoscapejs";
 import Cytoscape from "cytoscape";
+import Modal from "react-modal";
 
 require("dagre");
 
 Cytoscape.use(dagre);
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
+
 class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.myCyRef = React.createRef();
+
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = "#f00";
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   componentDidMount() {
@@ -45,6 +77,25 @@ class MyApp extends React.Component {
     ];
 
     return [
+      <button onClick={this.openModal}>Open Modal</button>,
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={subtitle => (this.subtitle = subtitle)}>Hello</h2>
+        <button onClick={this.closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>,
       <CytoscapeComponent
         elements={this.props.elements}
         className="game-editor"
@@ -58,6 +109,8 @@ class MyApp extends React.Component {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  Modal.setAppElement("#editor");
+
   const editor = document.getElementById("editor");
   const elements = JSON.parse(editor.dataset.elements);
   const beginningId = editor.dataset.beginningId;
