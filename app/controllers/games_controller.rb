05f2408a6e11bfa7f18b502fd0f4bebe9cc96262
Jakub Godawa <jgodawa@appriss.com>
2019-10-14 16:58:19 +0200

    take elements from db, add position only to nodes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,10 +22,9 @@ class GamesController < ApplicationController
 
   # GET /games/1/edit
   def edit
-    @nodes = @game.places_for_graph
-    @edges = @game.choices_for_graph
+    @elements = @game.elements
     @beginning_id = @game.beginning.id
-    # TODO pass the json from db instead of these
+
     render layout: false
   end
 
@@ -51,24 +50,17 @@ class GamesController < ApplicationController
   def update
     params.permit!
 
-    # {"modalIsOpen"=>false, "elements"=>[{"data"=>{"id"=>"4", "content"=>"The beginning of ddd..."}}], "questions"=>{"4"=>{"content"=>"The beginning of ddd...", "answers"=>[]}}, "editingNodeId"=>"4", "beginningId"=>"4", "positions"=>{"4"=>{"x"=>15, "y"=>15}}, "id"=>"2", "game"=>{}}
-    
     params["elements"].each do |element|
-      element["position"] = params["positions"][element["data"]["id"]]
+      unless element["data"]["id"].start_with?('edge')
+        element["position"] = params["positions"][element["data"]["id"]]
+      end
     end
 
-    @game.update!(elements: params["elements"])
-    render json: @game
-
-    # respond_to do |format|
-    #   if @game.update(game_params)
-    #     format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @game }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @game.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    if @game.update!(elements: params["elements"])
+      render json: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
   end
 
   # DELETE /games/1
