eccc181873ee2f445f9bdef3a83528dc1fbd8ec1
Michal Darda <mdarda@pm.me>
2019-10-16 15:51:06 +0200

    Elixir app WIP

--- takemefrom/config/dev.exs
+++ takemefrom/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :takemefrom, Takemefrom.Repo,
   username: "postgres",
   password: "postgres",
-  database: "takemefrom_dev",
+  database: "takemefrom_development",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
