f208649f796786335eddeba3f79cfd27f0c3189a
Michal Darda <mdarda@pm.me>
2019-10-23 11:31:48 +0200

    Test for authenticate by email and pass

--- test/takemefrom/accounts_test.exs
+++ test/takemefrom/accounts_test.exs
@@ -2,6 +2,7 @@ defmodule Takemefrom.AccountsTest do
   use Takemefrom.DataCase
 
   alias Takemefrom.Accounts
+  alias Takemefrom.Accounts.User
 
   describe "users" do
     @valid_attrs %{email: "some@email", password: "foobar"}
@@ -29,6 +30,13 @@ defmodule Takemefrom.AccountsTest do
     test "get_user_by/1 returns the user by given opts" do
       user = user_fixture()
       assert Accounts.get_user_by(email: user.email).email == user.email
+    end
+
+    test "authenticate_by_email_and_pass/2 should return ok if password match" do
+      user = user_fixture()
+
+      assert Accounts.authenticate_by_email_and_pass(user.email, "foobar") ==
+               {:ok, %User{user | password: nil}}
     end
   end
 end
