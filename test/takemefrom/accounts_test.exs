5f628f3ae434a7fc9e37498589e7e05bd565a438
Michal Darda <mdarda@pm.me>
2019-10-23 11:17:33 +0200

    Account tests

--- test/takemefrom/accounts_test.exs
+++ test/takemefrom/accounts_test.exs
@@ -15,9 +15,20 @@ defmodule Takemefrom.AccountsTest do
       user
     end
 
+    test "register_user/1 creates new user" do
+      {:ok, user} = Accounts.register_user(@valid_attrs)
+
+      assert user.email == @valid_attrs[:email]
+    end
+
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
       assert Accounts.get_user!(user.id).email == user.email
+    end
+
+    test "get_user_by/1 returns the user by given opts" do
+      user = user_fixture()
+      assert Accounts.get_user_by(email: user.email).email == user.email
     end
   end
 end
