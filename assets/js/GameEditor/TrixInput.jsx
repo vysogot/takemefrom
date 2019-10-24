b29fe9973942521a588fba7da7f4a745f0d2ad60
Michal Darda <mdarda@pm.me>
2019-10-24 23:18:44 +0200

    Add uploading files

--- assets/js/GameEditor/TrixInput.jsx
+++ assets/js/GameEditor/TrixInput.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Trix from "trix";
 import "trix/dist/trix.css";
+import AWS from "aws-sdk";
 
 export default class TrixInput extends React.Component {
   constructor(props) {
@@ -8,10 +9,69 @@ export default class TrixInput extends React.Component {
     this.trixInput = React.createRef();
   }
 
+  uploadAttachment = attachment => {
+    const albumBucketName = "takemefrom-dev";
+    const bucketRegion = "eu-central-1";
+    const IdentityPoolId = "eu-central-1:62d5c74d-2ab4-4224-81a0-7d6dc7a42dc8";
+
+    AWS.config.update({
+      region: bucketRegion,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      })
+    });
+
+    const s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      params: { Bucket: albumBucketName }
+    });
+
+    const albumName = "content";
+    const file = attachment.file;
+    const fileName = file.name;
+    const albumPhotosKey = encodeURIComponent(albumName) + "//";
+
+    const photoKey = fileName;
+
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file,
+        ACL: "public-read"
+      }
+    });
+
+    const promise = upload.promise();
+
+    promise.then(
+      function(data) {
+        attachment.setAttributes({
+          url: data.Location,
+          href: data.Location
+        });
+      },
+      function(err) {
+        console.log("There was an error uploading your photo: ", err.message);
+      }
+    );
+  };
+
   componentDidMount() {
+    Trix.config.attachments.preview.caption = {
+      name: false,
+      size: false
+    };
+
     this.trixInput.current.addEventListener("trix-change", event => {
-      console.log("trix change event fired");
       this.props.onChange(event.target.innerHTML);
+    });
+
+    this.trixInput.current.addEventListener("trix-attachment-add", event => {
+      const attachment = event.attachment;
+      if (attachment.file) {
+        return this.uploadAttachment(attachment);
+      }
     });
   }
 
