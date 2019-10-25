bf9db15165b06fc1b9fbd5fa5eb3180fb763ce65
Jakub Godawa <jakub.godawa@gmail.com>
2019-10-25 10:28:03 +0200

    sanitize trix choices, remove div

--- lib/takemefrom_web/views/play_view.ex
+++ lib/takemefrom_web/views/play_view.ex
@@ -1,8 +1,13 @@
 defmodule TakemefromWeb.PlayView do
   use TakemefromWeb, :view
   alias Takemefrom.Games
-
   def dead_end?(choices) do
     Enum.empty?(choices)
+  end
+
+  def sanitize({ :safe, html }) do
+    regex = ~r"block-->(.+?)<\/div>"
+    [_, inner] = Regex.run(regex, html)
+    raw(inner)
   end
 end
